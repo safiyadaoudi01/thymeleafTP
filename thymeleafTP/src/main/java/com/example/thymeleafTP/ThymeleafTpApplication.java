c1dfa3e47c6a433a35c5079334351e5b1c8f9526
safiyadaoudi01 <safidaou33@gmail.com>
2023-12-07 01:19:08 +0000

    summary
    12/07/2023

---
+++ thymeleafTP/src/main/java/com/example/thymeleafTP/ThymeleafTpApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTpApplication.class, args);
+	}
+
+}
---
+++ thymeleafTP/src/test/java/com/example/thymeleafTP/ThymeleafTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
